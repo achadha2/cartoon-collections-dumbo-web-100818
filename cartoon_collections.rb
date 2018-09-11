12f39289242a718667504d9a5c20ae8f8fbcf385
andy.chadha2@gmail.com <andy.chadha2@gmail.com>
2018-09-11 22:42:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(elements)
-  # Your code here
+  elements.map{|element| "#{element."
 end
 
 def long_planeteer_calls# code an argument here
